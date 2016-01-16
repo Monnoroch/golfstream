773256e2035b2c7dacac6b3dbc0e7fd01515ddd7
monnoroch <monnoroch@gmail.com>
2016-01-16 13:33:54 +0300

    added bufferedStream struct to replace the need for channel subscriber that was needed to ensure, that all the subscription events get added to subscriber after history events

--- backend/stream.go
+++ backend/stream.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/Monnoroch/golfstream/dchan"
 	"github.com/Monnoroch/golfstream/stream"
+	"sync/atomic"
 )
 
 /// Push interface for a stream of events
@@ -44,6 +45,66 @@ func Copy(drain stream.Stream, sink Stream) (error, bool) {
 		}
 
 		if err := sink.Add(val); err != nil {
+			return err, true
+		}
+	}
+	return nil, false
+}
+
+type bufferedStream struct {
+	base Stream
+
+	mode uint32
+	buf  []stream.Event
+}
+
+func (self *bufferedStream) Add(evt stream.Event) error {
+	if atomic.LoadUint32(&self.mode) == 0 {
+		self.buf = append(self.buf, evt)
+		return nil
+	}
+
+	if self.buf != nil {
+		for i, v := range self.buf {
+			if err := self.base.Add(v); err != nil {
+				// drop current event
+				self.buf = self.buf[:i+1]
+				return err
+			}
+		}
+		self.buf = nil
+	}
+
+	return self.base.Add(evt)
+}
+
+func (self *bufferedStream) AddDirect(evt stream.Event) error {
+	return self.base.Add(evt)
+}
+
+func (self *bufferedStream) Start() {
+	atomic.StoreUint32(&self.mode, 1)
+}
+
+func (self *bufferedStream) Close() error {
+	return self.base.Close()
+}
+
+func Buffered(s Stream, buf int) *bufferedStream {
+	return &bufferedStream{s, 0, make([]stream.Event, 0, buf)}
+}
+
+func CopyDirect(drain stream.Stream, sink *bufferedStream) (error, bool) {
+	for {
+		val, err := drain.Next()
+		if err == stream.EOI {
+			break
+		}
+		if err != nil {
+			return err, false
+		}
+
+		if err := sink.AddDirect(val); err != nil {
 			return err, true
 		}
 	}
